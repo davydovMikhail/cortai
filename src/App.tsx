b64206d1f17bf7ae02435001b43838934c65a5df
davydovMikhail <davydov27977@gmail.com>
2024-11-07 16:07:01 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,128 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { useActions } from './storeHooks/useActions';
+import VectorsWrapper from './images/Vectors-Wrapper.svg';
+import VectorsWrapper1 from './images/Vectors-Wrapper_1.svg';
+import VectorsWrapper2 from './images/Vectors-Wrapper_2.svg';
+import VectorsWrapper3 from './images/Vectors-Wrapper_3.svg';
+import VectorsWrapper4 from './images/Vectors-Wrapper_4.svg';
+import VectorsWrapper6 from './images/Vectors-Wrapper_6.svg';
+import VectorsWrapper7 from './images/Vectors-Wrapper_7.svg';
+import VectorsWrapper8 from './images/Vectors-Wrapper_8.svg';
+import VectorsWrapper9 from './images/Vectors-Wrapper_9.svg';
+import Girl from './images/Untitled1-1707x943-1.png';
 
 function App() {
   
 
   return (
     <>
-      <section className="section">
-        <div id="w-node-_438df5a7-ec38-3fa2-8541-5f4b6bc41c3d-61efa9ba" className="w-layout-layout quick-stack wf-layout-layout">
-          <div id="w-node-_438df5a7-ec38-3fa2-8541-5f4b6bc41c3e-61efa9ba" className="w-layout-cell cell">
-            <h1 className="heading">Tokenomics</h1>
+      <section className="section-2">
+        <section className="frame-2072749925">
+          <div className="frame-2072749926">
+            <div className="_1-2">
+              <div className="text-6">tokenomics</div>
+              <div className="frame-2072749927">
+                <div className="frame-2072749923">
+                  <div className="text-7">
+                    <div className="text-7 text-7"><span className="_84-liquidity-2">84%</span><span className="_84-liquidity-3"> Liquidity</span></div>
+                  </div>
+                  <div className="text-7">
+                    <div className="text-7"><span className="_10-bank-2">10% </span><span className="_84-liquidity-3"> bank</span></div>
+                  </div>
+                </div>
+                <div className="frame-2072749924">
+                  <div className="text-7">
+                    <div className="text-7"><span className="_10-bank-2">6% </span><span className="_84-liquidity-3">Team</span></div>
+                  </div>
+                  <a href="#" target="_blank" className="button-4">
+                    <div className="text-8">buy</div>
+                  </a>
+                </div>
+              </div><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2" />
+            </div>
+            <div className="_2-2">
+              <div className="text-9">contract</div>
+              {/* <img src="placeholder.svg" loading="lazy" alt="" /> */}
+              <div className="text-10">XXX...XXX</div>
+              <a href="#" className="button-5">
+                <div className="text-8">Copy</div>
+              </a><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2" />
+            </div>
+            <div className="_2-2 _3">
+              <div className="text-9">links</div>
+              <div className="tetx1">Unlock the potential of your projects with Generative GPU. </div>
+              <div className="_123-2">
+                <a href="#" className="button-6"><img src={VectorsWrapper6} loading="lazy" width="27.69230842590332" height="27.69230842590332" alt="" className="vectors-wrapper-8" /></a>
+                <a href="#" className="button-6"><img src={VectorsWrapper7} loading="lazy" width="27.69230842590332" height="27.69230842590332" alt="" className="vectors-wrapper-8" /></a>
+              </div><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2" />
+            </div>
           </div>
-          <div id="w-node-_438df5a7-ec38-3fa2-8541-5f4b6bc41c3f-61efa9ba" className="w-layout-cell cell-3">
-            <div className="w-layout-blockcontainer container w-container"></div>
-            <h1 className="heading main">Cortana AI - $cort</h1>
+          <div className="_4">
+            <div className="text-12">$CORT</div><img src={ Girl } loading="lazy" width="301" height="736" alt="" sizes="(max-width: 479px) 83vw, 301px" className="untitled-1-1707x943-1" />
+            <div className="frame-2072749919">
+              <div className="text-11">CORTANA AI</div>
+              <div className="tetx1">Unlock the potential of your projects with Generative GPU. Gain unparalleled experience in GPU Computing, rent high-performance nodes, and utilize cutting-edge streaming servers.</div>
+            </div><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2 _223" /><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2" />
           </div>
-          <div id="w-node-f009a186-84ea-db52-5fb9-54849d141fa9-61efa9ba" className="w-layout-cell chat">
-            <h1 className="heading">user info</h1>
+          <div className="frame-2072749922">
+            <div className="_5">
+              <div className="text-13">$CORT study progression</div>
+              <div className="_69">69% </div><img src={VectorsWrapper1} loading="lazy" width="228" height="161" alt="" className="vectors-wrapper-11" /><img src={VectorsWrapper8} loading="lazy" alt="" className="image" /><img src={VectorsWrapper2} loading="lazy" width="155" height="155" alt="" className="vectors-wrapper-12" /><img src={ VectorsWrapper } loading="lazy" alt="" className="image-2" />
+            </div>
+            <div className="_6">
+              <div className="message">
+                <div className="frame-48096125">
+                  <div className="frame-48096124"><img src={VectorsWrapper9} loading="lazy" width="48" height="48.000003814697266" alt="" className="vectors-wrapper-13" />
+                    <div className="text-8">Cortana chat</div><img src={ VectorsWrapper3 } loading="lazy" width="26" height="26" alt="" className="vectors-wrapper-14" />
+                  </div>
+                </div>
+                <div className="frame-2072749665">
+                  <div className="frame-48096132"><img src={ VectorsWrapper4 } loading="lazy" width="24" height="24" alt="" className="vectors-wrapper-15" /></div>
+                </div>
+              </div>
+              <div className="frame-2072749680">
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="message-2">
+                  <div className="text-14">Yoooo</div>
+                </div>
+                <div className="frame-2072749679">
+                  <div className="message-3">
+                    <div className="text-14">Hello</div>
+                  </div>
+                  <div className="message-4">
+                    <div className="text-14">Can I help you?</div>
+                  </div>
+                </div>
+              </div>
+              <div className="div-block">
+                <div className="w-form">
+                  <form id="email-form" name="email-form" data-name="Email Form" method="get" className="form-2" data-wf-page-id="6717b20bef0a0bfd61efa9ba" data-wf-element-id="6c10236e-2f11-ba2a-50d6-560a228f000f"><input className="text-field-2 w-input" name="field" data-name="Field" placeholder="Message..." type="text" id="field" /><input type="submit" data-wait="Please wait..." className="submit-button-2 w-button" value="Send" /></form>
+                  <div className="w-form-done">
+                    <div>Thank you! Your submission has been received!</div>
+                  </div>
+                  <div className="w-form-fail">
+                    <div>Oops! Something went wrong while submitting the form.</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div id="w-node-_7faf3c8f-5074-3892-6742-42bd61e9560e-61efa9ba" className="w-layout-cell cell">
-            <h1 className="heading">contract</h1>
-          </div>
-          <div id="w-node-_948ec0e0-45f5-d4b8-747c-a3cfd2c033f7-61efa9ba" className="w-layout-cell cell-user">
-            <h1 className="heading">Chat ai</h1>
-          </div>
-          <div id="w-node-_53038b5b-c0e3-ea68-504e-ece5171b7ddd-61efa9ba" className="w-layout-cell cell">
-            <h1 className="heading">links</h1>
-          </div>
-        </div>
+        </section>
       </section>
       <ToastContainer/>
     </>
